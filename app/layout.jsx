84cad91def9c45e68fd803f0f69e2e788f2d3dd1
222pl <nowakpawelnowak37@gmail.com>
2026-05-13 20:45:25 +0200

    fix layout final

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,7 @@
-export const metadata = {
-  title: 'CFD Quiz',
-  description: 'Quiz inwestycyjny CFD',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
       <body>{children}</body>
     </html>
-  )
+  );
 }
